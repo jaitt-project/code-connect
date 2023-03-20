38c384db2dd42af6de16fa9abc9e20bfc00619e0
Ahmed Chami <achami8799@gmail.com>
2023-03-20 19:20:03 -0400

    Creates server boilerplate

---
+++ server/controllers/authController.js
@@ -0,0 +1,13 @@
+const authController = {};
+
+authController.authenticate = () => {
+  fetch('https://github.com/login/oauth/authorize', {
+    method: 'GET',
+    client_id: 'c39c3106c66253bf31bc',
+    redirect_uri: '/',
+    headers: 'Content-type: application/json',
+  }).then((res) => res.json())
+    .then((data) => console.log(data));
+};
+
+module.exports = authController;
